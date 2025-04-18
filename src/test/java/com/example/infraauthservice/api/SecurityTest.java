54a4ae21f8a1e94bf31680a602f6e3ece9de2d2d
seculoper <mir8725@naver.com>
2025-04-18 18:17:50 +0900

    API 목표 명시

---
+++ src/test/java/com/example/infraauthservice/api/SecurityTest.java
@@ -0,0 +1,19 @@
+package com.example.infraauthservice.api;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SecurityTest {
+
+    @Test
+    @DisplayName("JWT 인증")
+    public void email_sign_up() {
+        //
+    }
+
+    @Test
+    @DisplayName("토큰 재발급")
+    public void reissue_token() {
+        //
+    }
+}
